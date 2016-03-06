460b0d1453539b31cbeb0ec69aae831d83b9df9d
Pawel B.B. Drozd <bodolsog@gmail.com>
2016-03-06 21:53:20 +0100

    Lista obecności.

---
+++ app/src/main/java/pl/angrymarschmallow/testapp/MainActivity.java
@@ -0,0 +1,21 @@
+package pl.angrymarschmallow.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> listaObecnosci = new ArrayList<String>(){{
+        add("bodolsog");
+
+
+    }};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
